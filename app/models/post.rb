f0c5826066d8362f3e0e7aa19c23325789e39bb6
Pedro_Atalaia <pedrocondatalaia@hotmail.com>
2022-06-03 16:42:38 -0400

    feat: added friendly_id to Post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ApplicationRecord
+
+    extend FriendlyId
+    friendly_id :slug, use: :slugged
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_03_203542) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_03_204053) do
   create_table "featured_contents", force: :cascade do |t|
     t.string "title"
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "friendly_id_slugs", force: :cascade do |t|
+    t.string "slug", null: false
+    t.integer "sluggable_id", null: false
+    t.string "sluggable_type", limit: 50
+    t.string "scope"
+    t.datetime "created_at"
+    t.index ["slug", "sluggable_type", "scope"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type_and_scope", unique: true
+    t.index ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type"
+    t.index ["sluggable_type", "sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_type_and_sluggable_id"
   end
 
   create_table "photos", force: :cascade do |t|
@@ -28,9 +39,10 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_03_203542) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
-    t.string "slug"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "slug"
+    t.index ["slug"], name: "index_posts_on_slug", unique: true
   end
 
   add_foreign_key "photos", "featured_contents", column: "featured_contents_id"
